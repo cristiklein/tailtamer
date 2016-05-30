0b3e241e5eb2ebe0bd86162d3b7174da18bb6a6d
Cristian Klein <cristiklein@gmail.com>
2016-05-30 11:23:36 +0200

    Continued prototying OpenLoopClient and MicroService

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ Main module of the simulator. Processes input to simulation, steers simulation a
 import collections
 import csv
 from itertools import tee
-import random
 
 Result = collections.namedtuple('Result', 'arrival_rate method response_time')
 
@@ -26,13 +25,37 @@ class Environment(object):
     TODO, likely with SimPy
     """
     def __init__(self):
-        pass
+        import random as EnvironmentRandom # avoid accidental usage
+        self._random = EnvironmentRandom.Random()
+        # TODO: Seed should be a command-line parameter to allow sensitivity testing.
+        self._random.seed(1)
 
     def run(self):
         """
         Runs the simulation, either to completion or up to a given time.
         """
         pass
+
+    def spawn(self, function):
+        """
+        Schedule a method to be executed.
+        """
+        pass
+
+    @property
+    def now(self):
+        """
+        Returns the current simulator time.
+        """
+        pass
+
+    @property
+    def random(self):
+        """
+        Get a pseudo-random number generator. Prefer this method, instead of
+        instantiating a `random` directly, to ensure the simulator is idempotent.
+        """
+        return self._random
 
 class PhysicalMachine(object):
     """
@@ -62,40 +85,88 @@ class VirtualMachine(object):
         # TODO
         pass
 
+class Request(object):
+    """
+    Represents a request, travelling horizontally and vertically through the system.
+    Only the client is allowed to create new requests.
+    """
+    def __init__(self, start_time):
+        self._start_time = start_time
+        self._end_time = None
+
+    @property
+    def start_time(self):
+        return self._start_time
+
+    def get_end_time(self):
+        return self._end_time
+
+    def set_end_time(self, new_value):
+        assert self._end_time is None
+        self._end_time = new_value
+
+    end_time = property(get_end_time, set_end_time)
+
 class OpenLoopClient(object):
     """
     Simulates open-loop clients, with a given arrival rate.
     """
     def __init__(self, env, arrival_rate):
-        # TODO
-        self.arrival_rate = arrival_rate
-        self.env = env
+        self._arrival_rate = arrival_rate
+        self._env = env
+        self._downstream_microservice = None
+        self._requests = []
 
-    def connect_to(self, caller):
-        # TODO
-        pass
+        self._env.spawn(self.run)
+
+    def connect_to(self, microservice):
+        self._downstream_microservice = microservice
+
+    def run(self):
+        while True:
+            self._env.spawn(self._on_arrival)
+            waiting_time = self._env.random.expovariate(self._arrival_rate)
+            self._env.sleep(waiting_time)
+
+    def _on_arrival(self):
+        request = Request(start_time=self._env.now)
+        self._requests.append(request)
+        self._downstream_microservice.on_request(request)
+        request.end_time = self._env.now
 
     @property
     def response_times(self):
-        mean_response_time = 1.0
-        return [random.expovariate(1.0/mean_response_time*self.arrival_rate)
-                for _ in range(0, 1000)]
+        return [r.end_time - r.start_time for r in self._requests]
 
 class MicroService(object):
     """
     Simulates a micro-service, with a given average work and downcall structure.
     """
     def __init__(self, env, average_work, thread_pool_size=100):
-        # TODO
-        pass
+        self._env = env
+        self._average_work = average_work
+        self._executor = None
+        self._downstream_microservices = []
 
     def run_on(self, executor):
-        # TODO
-        pass
+        self._executor = executor
 
-    def connect_to(self, caller):
-        # TODO
-        pass
+    def connect_to(self, microservice):
+        self._downstream_microservices.append(microservice)
+
+    def on_request(self, request):
+        with request.trace(self):
+            # TODO: Add randomness to demand. Variance might be a command-line parameter.
+            demand = self._average_work
+            demand_between_calls = demand / (len(self._downstream_microservices)+1)
+
+            self._compute(request, demand_between_calls)
+            for microservice in self._downstream_microservices:
+                microservice.on_request(request)
+                self._compute(request, demand_between_calls)
+
+    def _compute(self, request, demand):
+        self._executor.execute(request, demand)
 
 def run_simulation(arrival_rate, method, physical_machines=1):
     """
@@ -147,7 +218,7 @@ def run_simulation(arrival_rate, method, physical_machines=1):
         for microservice in layer:
             virtual_machine = VirtualMachine(env, num_cpus=2)
             microservice.run_on(virtual_machine)
-            # TODO: Mapping algorithm
+            # TODO: Optionally add a VM to PM mapping algorithm.
             virtual_machine.run_on(physical_machines[0])
             virtual_machines.append(virtual_machine)
 
