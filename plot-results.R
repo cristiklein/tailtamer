e3a7322b7a5e96b48191300915053442282fd18e
Cristian Klein <cristiklein@gmail.com>
2016-05-30 15:44:34 +0200

    Remove confusing violin plot ends

--- plot-results.R
+++ plot-results.R
@@ -4,7 +4,7 @@ data$arrival_rate <- as.factor(data$arrival_rate)
 library(ggplot2)
 dodge <- position_dodge(width = 0.7)
 p <- ggplot(data, aes(x=arrival_rate, y=response_time, fill=method)) +
-  geom_violin(position = dodge, trim=FALSE) +
+  geom_violin(position = dodge) +
   labs(title="Simulation Results", x="Arrival rate [requests/s]", y = "Response time [s]") +
   geom_point(stat = "summary", fun.y = "mean", color = "black", position = dodge)
 print(p)
