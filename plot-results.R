7f5b73ae6bb3d94d2341f461a2ddba73fb1d647d
Cristian Klein <cristiklein@gmail.com>
2016-06-01 17:02:56 +0200

    Include git commit in plot

--- plot-results.R
+++ plot-results.R
@@ -1,10 +1,16 @@
+git_dirty <- (length(system('git status --untracked-files=no --porcelain', intern=TRUE)) > 0)
+git_commit <- system('git log -1 --format="%H"', intern=TRUE)
+
 data <- read.csv('results.csv')
 data$arrival_rate <- as.factor(data$arrival_rate)
+
+plot_title = sprintf('Simulation Results (%s%s)', git_commit, if (git_dirty) '+' else '')
 
 library(ggplot2)
 dodge <- position_dodge(width = 0.7)
 p <- ggplot(data, aes(x=arrival_rate, y=response_time, fill=method)) +
   geom_violin(position = dodge) +
-  labs(title="Simulation Results", x="Arrival rate [requests/s]", y = "Response time [s]") +
+  labs(title=plot_title, x="Arrival rate [requests/s]", y = "Response time [s]") +
   geom_point(stat = "summary", fun.y = "mean", color = "black", position = dodge)
 print(p)
+
