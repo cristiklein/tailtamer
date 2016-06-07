0d9a2b5c9c88478140d19080ab6d974c32352392
Cristian Klein <cristiklein@gmail.com>
2016-06-07 14:21:59 +0200

    Added dots for 95th and 99th percentile

--- plot-results.R
+++ plot-results.R
@@ -6,11 +6,20 @@ data$arrival_rate <- as.factor(data$arrival_rate)
 
 plot_title = sprintf('Simulation Results (%s%s)', git_commit, if (git_dirty) '+' else '')
 
+p95 <- function(values) {
+  quantile(values, .95)
+}
+
+p99 <- function(values) {
+  quantile(values, .99)
+}
+
 library(ggplot2)
 dodge <- position_dodge(width = 0.7)
 p <- ggplot(data, aes(x=arrival_rate, y=response_time, fill=method)) +
   geom_violin(position = dodge) +
   labs(title=plot_title, x="Arrival rate [requests/s]", y = "Response time [s]") +
-  geom_point(stat = "summary", fun.y = "mean", color = "black", position = dodge)
+  geom_point(stat = "summary", fun.y = "mean", position = dodge, size = 3, show.legend = FALSE) +
+  geom_point(stat = "summary", fun.y = "p95" , position = dodge, size = 2, show.legend = FALSE) +
+  geom_point(stat = "summary", fun.y = "p99" , position = dodge, show.legend = FALSE)
 print(p)
-
