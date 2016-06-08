d82a486678549ea147787d2f409cfee48626b69a
Cristian Klein <cristiklein@gmail.com>
2016-06-08 15:47:40 +0200

    Erase y-axis padding

--- plot-results.R
+++ plot-results.R
@@ -1,7 +1,7 @@
 git_dirty <- (length(system('git status --untracked-files=no --porcelain', intern=TRUE)) > 0)
 git_commit <- system('git log -1 --format="%H"', intern=TRUE)
 
-data <- read.csv('results.csv')
+data <- read.csv('results-ar.csv')
 data$arrival_rate <- as.factor(data$arrival_rate)
 
 plot_title = sprintf('Simulation Results (%s%s)', git_commit, if (git_dirty) '+' else '')
@@ -17,6 +17,7 @@ p99 <- function(values) {
 library(ggplot2)
 dodge <- position_dodge(width = 0.7)
 p <- ggplot(data, aes(x=arrival_rate, y=response_time, fill=method)) +
+  scale_y_continuous(limits=c(0,1), expand = c(0, 0)) +
   geom_violin(position = dodge) +
   labs(title=plot_title, x="Arrival rate [requests/s]", y = "Response time [s]") +
   geom_point(stat = "summary", fun.y = "mean", position = dodge, size = 3, show.legend = FALSE) +
