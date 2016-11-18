2afc74eb36cbc0f5416c1e4860e13843a6a3e1ba
Cristian Klein <cristiklein@gmail.com>
2016-11-18 16:54:58 +0100

    Export some TailTamer functionality

--- tailtamer/__init__.py
+++ tailtamer/__init__.py
@@ -1,1 +1,4 @@
-# Nothing yet
+from tailtamer.request import Request, Work
+from tailtamer.machine import VirtualMachine, PhysicalMachine
+from tailtamer.simulation import NsSimPyEnvironment
+from tailtamer.thread import Thread
